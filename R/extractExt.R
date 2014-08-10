9a1a6dd15a4f54c953cab6a26928ca3976f22af6
gisma <giswerk@gis-ma.org>
2014-08-10 23:07:48 +0200

    improved calculations

--- R/extractExt.R
+++ R/extractExt.R
@@ -1,4 +1,4 @@
-extractExt=function(file,proj,lat,lon){
+extractExt=function(file){
 
   #
   # Copyright 2013 Hanna Meyer, and Chris Reudenbach
@@ -20,20 +20,30 @@ extractExt=function(file,proj,lat,lon){
   # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
   #  
   # extractExt identifies the extension of an arbitrary ARPS model run ut in netCDF format
- 
   
+  # t<-GDALinfo("inst/allgaeu_d1_ARP.nc" ,returnScaleOffset=FALSE, returnRAT=TRUE)
+  #dx=as.numeric(strsplit(attributes(t)$mdata[13][which(grepl(attributes(t)$mdata[13], attributes(t)$mdata[13]))],"=")[[1]][2])
+  tmp=extractProj(file)
+  proj=tmp$proj
+  lat0=tmp$lat
+  lon0=tmp$lon
+  dx=tmp$dx
+  dy=tmp$dy
   testncdf=open.ncdf(file)
   x = length(get.var.ncdf( testncdf, "x"))
   y = length(get.var.ncdf( testncdf, "y"))
-  xmin=11.1332
-  xmax=xmin+(x*0.0269)
-  ymin=44.5809
-  ymax=ymin+(y*0.0181)
-  write.table(matrix(c(xmin,ymin,xmax,ymax),ncol=2,nrow=2,byrow=T),"aRpsIn.txt",row.names=FALSE,col.names=FALSE)
-  system(paste("gdaltransform -s_srs EPSG:4326  -t_srs ","'", proj,"'"," < aRpsIn.txt > aRpsOut.txt"))
-  tmp=read.table("aRpsOut.txt")
-  ext=c(tmp[1,1],tmp[2,1],tmp[1,2],tmp[2,2])
-  system("rm aRpsOut.txt")
-  system("rm aRpsIn.txt")
+  write.table(cbind(lat0,lon0),'incclatlon.txt',row.names=FALSE,col.names=FALSE,sep=' ')
+  system(paste("gdaltransform -s_srs EPSG:4326  -t_srs '", proj,"' < incclatlon.txt > outcclcc.txt"))
+  tmp=read.table("outcclcc.txt")
+  xmin=tmp[1,2]-x/2*dx
+  xmax=tmp[1,2]+x/2*dx
+  ymin=tmp[1,1]-y/2*dy
+  ymax=tmp[1,1]+y/2*dy
+  #write.table(matrix(c(xmin,ymin,xmax,ymax),ncol=2,nrow=2,byrow=T),"aRpsIn.txt",row.names=FALSE,col.names=FALSE)
+  #system(paste("gdaltransform -s_srs EPSG:4326  -t_srs ","'", proj,"'"," < aRpsIn.txt > aRpsOut.txt"))
+  #tmp=read.table("aRpsOut.txt")
+  ext=c(xmin,xmax,ymin,ymax)
+  system("rm incclatlon.txt")
+  system("rm outcclcc.txt")
   return (ext)
 }
